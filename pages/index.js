33576e42dc93d9654e4043bc422af711b7d9dcda
Sidney Vitório <60122151+sidneyweb@users.noreply.github.com>
2021-10-26 10:31:41 -0300

    teste funça kink do Netx.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 function Home() {
-    return <div>Home page index Estudo</div>
+    return (<div>
+         <h1>Home page - index</h1>
+
+        <Link href="/sobre">
+            <a>Acessar a página Sobre</a>
+        </Link>
+        </div>)
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function Sobre() {
+    return (<div>
+         <h1>Sobre Nós pagina sobre.js</h1>
+
+        <Link href="/">
+            <a>Acessar a página Home</a>
+        </Link>
+        </div>)
+}
+
+export default Sobre
